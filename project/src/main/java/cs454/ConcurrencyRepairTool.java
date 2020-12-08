d708d663e7b917447f2f80f835ebbb23ab4601bb
dfbsdan <dfbsdaniel11@hotmail.com>
2020-12-08 17:15:45 +0900

    Further improvements

--- project/src/main/java/cs454/ConcurrencyRepairTool.java
+++ project/src/main/java/cs454/ConcurrencyRepairTool.java
@@ -1,6 +1,6 @@
 package cs454;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////TEMPORARY CODE(?)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////TEMPORARY CODE(?)
 
 import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.ast.stmt.IfStmt;
@@ -8,7 +8,7 @@ import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.utils.CodeGenerationUtils;
@@ -20,6 +20,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService; 
+import java.util.concurrent.Executors; 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 /**
  * Main Class
@@ -28,20 +33,17 @@ public class ConcurrencyRepairTool
 {
     public static void main( String[] args )
     {
-        if(args.length != 4) { //////////////////////////////////////////////////////////////////////////////////////////////////MAY CHANGE
+        if(args.length != 6) { ////////////////////////////////////////////////////////////////////////////////////////MAY CHANGE
             throw new IllegalArgumentException("Wrong amount of arguments.");
         }
-        // Population size (per program repaired)
         int populationSize = Integer.parseInt(args[0]);
         if(populationSize <= 0) {
             throw new IllegalArgumentException("The population size has to be greater than 0.");
         }
-        // Number of fitness evaluations (per program repaired)
         int fitnessEvaluations = Integer.parseInt(args[1]);
         if(fitnessEvaluations <= 0) {
             throw new IllegalArgumentException("The amount of fitness evaluations has to be greater than 0.");
         }
-        // Percentage of offsprings per generation (per program repaired)
         float offsprings = Float.parseFloat(args[2]);
         if(offsprings <= 0 || offsprings > 1) {
             throw new IllegalArgumentException("Incorrect offspring percentage.");
@@ -50,24 +52,29 @@ public class ConcurrencyRepairTool
         if(mutationCnt < 0) {
             throw new IllegalArgumentException("The amount of mutations has to be greater or equal to 0.");
         }
+        int poolSize = Integer.parseInt(args[4]);
+        if(poolSize <= 0) {
+            throw new IllegalArgumentException("The thread pool size has to be greater than 0.");
+        }
+        int timeout = Integer.parseInt(args[5]);
+        if(timeout <= 0) {
+            throw new IllegalArgumentException("The timeout has to be greater than 0");
+        }
 
         // JavaParser has a minimal logging class that normally logs nothing.
         // Let's ask it to write to standard out:
         Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
 
-        ReparationTool repTool = new ReparationTool(populationSize, fitnessEvaluations, offsprings, mutationCnt);
-
-        // Repair all sources
-        for(String src : repTool.sources)
-            repTool.repair(src);
-
-        // Finish
-        repTool.saveAll();
+        ReparationTool repTool = new ReparationTool(populationSize, fitnessEvaluations, offsprings, mutationCnt, poolSize, timeout);
+        repTool.repairAll();
+        repTool.finish();
     }
 }
 
 class ReparationTool
 {
+    // Threadpool used to perform fitness evaluations
+    private final ExecutorService pool;
     // The path of the Maven module/project which contains the ConcurrencyRepairTool class.
     // appended with a path to "output".
     private final String outputFile = CodeGenerationUtils.mavenModuleRoot(ConcurrencyRepairTool.class)
@@ -78,13 +85,14 @@ class ReparationTool
     // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
     // In this case the root directory is found by taking the root from the current Maven module,
     // with src/main/sources appended.
-    private SourceRoot sourceRoot;
-    private int popSize; // Population size (per improved program)
-    private int fitnessEval; // Number of fitness evaluations (per improved program)
-    private int offspringCnt; // Number of new offsprings per generation (per program)
-    private int mutationCnt; // Number of mutations performed per new offspring
+    private final SourceRoot sourceRoot;
+    private final int popSize; // Population size (per improved program)
+    private final int fitnessEval; // Number of fitness evaluations (per improved program)
+    private final int offspringCnt; // Number of new offsprings per generation (per program)
+    private final int mutationCnt; // Number of mutations performed per new offspring
+    private final int timeout; // Timeout of a fitness evaluation
 
-    ReparationTool(int popSize, int fitnessEval, float offsprings, int mutationCnt) {
+    ReparationTool(int popSize, int fitnessEval, float offsprings, int mutationCnt, int poolSize, int timeout) {
         Path sourceRoot = CodeGenerationUtils.mavenModuleRoot(ConcurrencyRepairTool.class)
             .resolve("src/main/sources");
         try {
@@ -93,78 +101,145 @@ class ReparationTool
         } catch(IOException e) {
             e.getCause();
         }
+        this.pool = Executors.newFixedThreadPool(poolSize);
         this.sourceRoot = new SourceRoot(sourceRoot);
         this.popSize = popSize;
         this.fitnessEval = fitnessEval;
-        this.offspringCnt = (int)(offsprings * popSize);
-        if (this.offspringCnt == 0)
-            this.offspringCnt = 1; // At least one offspring per generation
+        int offspringCnt = (int)(offsprings * popSize);
+        this.offspringCnt = (offspringCnt > 0)? offspringCnt: 1; // At least one offspring per generation
         this.mutationCnt = mutationCnt;
+        this.timeout = timeout;
     }
 
-    public void repair(String src)
+    public void repairAll()
+    {
+        // Repair all sources
+        for(String src : sources)
+            repair(src);
+    }
+
+    private void repair(String src)
     {
         CompilationUnit cu = this.sourceRoot.parse("", src);
         Log.info("Repairing: " + src);
 
-        // Create the population (size constrained by fitness evaluation limit)
-        int popSize = (this.popSize < this.fitnessEval)? this.popSize: this.fitnessEval;
-        Solution population[] = new Solution[popSize]; // Sorted array of solutions (by fitness)
-        // Generate the initial population
-        for (int i = 0; i < popSize; i++)
-            population[i] = new Solution(cu.clone());
-        Arrays.sort(population, Solution::compare);
-        int fitnessEval = this.fitnessEval - popSize;
-        // Continue until all fitness evaluations are performed
-        while (fitnessEval > 0)
-            fitnessEval -= this.nextGeneration(population);
-        // Save the best solution
-        this.sourceRoot.add(this.outputFile, src, population[0].cu);
+        Population pop = new Population(popSize, fitnessEval, offspringCnt, mutationCnt, timeout, pool, cu);
+        pop.evolve();
+        this.sourceRoot.add(this.outputFile, src, pop.bestSolution().cu);
     }
 
-    private int nextGeneration(Solution population[]) /////////////////////////////////////////////////////////////////////////NOT FINISHED
+    // This saves all the files we just repaired to an output directory. 
+    public void finish()
+    {
+        this.sourceRoot.saveAll();
+        pool.shutdownNow();
+    }
+}
+
+class Population
+{
+    // List of pending fitness evaluations
+    private final List<FitnessEvaluator> tasks = new ArrayList<>();
+    private final int size; // Population size
+    private int fitnessEval; // Remaining fitness evaluations
+    private final int offspringCnt; // Max. number of new offsprings per generation
+    private final int mutationCnt; // Number of mutations performed per new offspring
+    private final Solution solutions[]; // Sorted array of solutions (by fitness)
+    private final ExecutorService pool; // Threadpool used for fitness evaluations
+    private final int timeout; // Timeout of a fitness evaluation
+
+    Population(int popSize, int fitnessEval, int offspringCnt, int mutationCnt, int timeout, ExecutorService pool, CompilationUnit cu)
+    {
+        size = (popSize < fitnessEval)? popSize: fitnessEval;
+        this.fitnessEval = fitnessEval - size;
+        this.offspringCnt = offspringCnt;
+        this.mutationCnt = mutationCnt;
+        this.timeout = timeout;
+        this.pool = pool;
+        solutions = new Solution[size];
+        // Generate the initial population
+        for (int i = 0; i < size; i++)
+            solutions[i] = new Solution(cu.clone(), tasks, timeout);
+        evaluateSolutions();
+        Arrays.sort(solutions, Solution::compare);
+        //printSolutions(); // Testing purposes
+    }
+
+    // Evolve until all fitness evaluations have been performed
+    public void evolve()
+    {
+        while (fitnessEval > 0)
+            fitnessEval -= nextGeneration();
+    }
+
+    private int nextGeneration() //////////////////////////////////////////////////////////////////////////////////////NOT FINISHED
     {
         // Set the number of offsprings to be generated (Constrained by fitness evaluation limit)
-        int offspringCnt = (this.offspringCnt < this.fitnessEval)? this.offspringCnt: this.fitnessEval;
+        int offspringCnt = (this.offspringCnt < fitnessEval)? this.offspringCnt: fitnessEval;
         // Generate the offsprings
         Solution offsprings[] = new Solution[offspringCnt];
         for (int i = 0; i < offspringCnt; i++) {
-            int parents[] = this.getParents();
-            offsprings[i] = new Solution(population, parents[0], parents[1]);
+            int parents[] = getParents();
+            offsprings[i] = new Solution(solutions[parents[0]].cu, solutions[parents[1]].cu, tasks, timeout);
         }
-        // Update the population
+        evaluateSolutions();
+        //printSolutions(); // Testing purposes
+        // TODO: Update the population
+
 
         return offspringCnt;
     }
 
-    private int[] getParents() ////////////////////////////////////////////////////////////////////////////////////////////////NOT FINISHED
+    private int[] getParents() ////////////////////////////////////////////////////////////////////////////////////////NOT FINISHED
     {
-        int parents[] = new int[2]; // Parent indexes inside the population
+        // TODO: Choose parents (their index inside the solutions list)
         //Log.info("Choosing parents");
+        int parents[] = new int[2];
+        parents[0] = 1;
+        parents[1] = 2;
+
 
         return parents;
     }
 
-    // This saves all the files we just repaired to an output directory. 
-    public void saveAll()
+    // Return the best solution found so far
+    public Solution bestSolution()
     {
-        this.sourceRoot.saveAll();
+        return solutions[size-1];
+    }
+
+    // Sets the fitness for all solutions that have not been evaluated
+    private void evaluateSolutions()
+    {
+        try {
+            pool.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        tasks.clear();
+    }
+
+    private void printSolutions() // Testing purposes
+    {
+        for(int i = 0; i < solutions.length; i++)
+            Log.info("Solution: " + i + ", fitness: " + solutions[i].fitness);
     }
 }
 
 class Solution
 {
-    public CompilationUnit cu;
+    public final CompilationUnit cu;
     public int fitness;
 
     // 'Random' solution generator
-    Solution(CompilationUnit cu) //////////////////////////////////////////////////////////////////////////////////////////////NOT FINISHED
+    Solution(CompilationUnit cu, List<FitnessEvaluator> tasks, int timeout) ///////////////////////////////////////////NOT FINISHED
     {
         //Log.info("Creating 'random' solution");
-        this.cu = cu; // Copy of the original
-        // Initialize
+        // TODO: Initialize
         
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////SAMPLE JAVAPARSER CODE
+        this.cu = cu; // Copy of the original (testing only)
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////SAMPLE JAVAPARSER CODE (TESTING PURPOSES ONLY)
         /*
         cu.accept(new ModifierVisitor<Void>() {
              // For every if-statement, see if it has a comparison using "!=".
@@ -188,33 +263,56 @@ class Solution
             }
         }, null);
         */
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // Evaluate
-        this.getFitness();
+        getFitness(tasks, timeout);
     }
 
-    // Creates a solution based on two given parents (i.e. their indexes inside
-    // the population array)
-    Solution(Solution population[], int p1, int p2) ///////////////////////////////////////////////////////////////////////////NOT FINISHED
+    // Creates a solution based on two given parents (i.e. their compilation units)
+    Solution(CompilationUnit p1, CompilationUnit p2, List<FitnessEvaluator> tasks, int timeout) ///////////////////////NOT FINISHED
     {
         //Log.info("Creating offspring solution");
-        // Inherit
+        // TODO: Inherit
+        cu = p1.clone();
 
-        // Mutate
+        // TODO: Mutate
 
         // Evaluate
-        this.getFitness();
+        getFitness(tasks, timeout);
     }
 
-    private void getFitness()//////////////////////////////////////////////////////////////////////////////////////////////////NOT FINISHED
+    // Adds the current solution to the tasks list so that its fitness can be
+    // evaluated later on by the population object
+    private void getFitness(List<FitnessEvaluator> tasks, int timeout)
     {
-        //Log.info("Evaluating solution");
+        tasks.add(new FitnessEvaluator(this, timeout));
     }
 
     // Comparator used for keeping the population array sorted
     public static int compare(Solution a, Solution b)
     {
         return b.fitness - a.fitness;
+    }
+}
+
+class FitnessEvaluator implements Callable<Void>
+{
+    private final Solution sol;
+    private int timeout;
+    static int bufFitness = 0; ////////////////////////////////////////////////////////////////////////////////////////Testing purposes (should be deleted)
+
+    FitnessEvaluator(Solution sol, int timeout)
+    {
+        this.sol = sol;
+        this.timeout = timeout;
+    }
+
+    public Void call() ////////////////////////////////////////////////////////////////////////////////////////////////NOT FINISHED
+    {
+        // TODO: Evaluate the solution and set its fitness
+        sol.fitness = bufFitness;
+        bufFitness += 10; // Testing purposes
+        return null;
     }
 }
